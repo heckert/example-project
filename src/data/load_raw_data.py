cf9c63247a79f28cf02e70a1d755cd2fbfa1b65c
Heinz Eckert <heinz_eckert@hotmail.com>
2021-05-02 18:32:09 +0200

    included Makefile & setup.py to create environment

--- src/data/load_raw_data.py
+++ src/data/load_raw_data.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
 import pandas as pd
 
-import src.globals
-from src.globals import raw_data_dir
-
-
-data_source = {'name': 'example.csv',
-            'url': '',
-            'meta_data_url': ''}
-
+from src.globals import raw_data_dir, interim_data_dir, processed_data_dir,\
+    data_source, logging
 
 def main(input_url, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """ 
+    Loads raw csv file from url specified in globals.py
+    Loaded file is stored in data directory
     """
     #df = pd.read_csv(input_url)
     #df.to_csv(output_filepath, index=False, encoding='utf8')
@@ -22,6 +15,12 @@ def main(input_url, output_filepath):
     
 
 if __name__ == '__main__':
+    for dir_ in [raw_data_dir, interim_data_dir, processed_data_dir]:
+        if not dir_.exists():
+            dir_.mkdir(parents=True)
+            logging.debug(f'{dir_} created')
+        else:
+            logging.debug(f'{dir_} exists')
     logging.info('loading raw data from {}'.format(data_source['url']))
     main(data_source['url'], raw_data_dir/data_source['name'])
     logging.info('data stored at {}'.format(raw_data_dir/data_source['name']))
--- src/globals.py
+++ src/globals.py
@@ -10,7 +10,7 @@ logging.basicConfig(format=log_fmt, datefmt=log_datefmt, level=log_defaultlvl)
 # DIRECTORIES
 project_dir = Path(__file__).resolve().parents[1]
 raw_data_dir = project_dir / 'data' / 'raw'
-interim_data_sir = project_dir / 'data' / 'interim'
+interim_data_dir = project_dir / 'data' / 'interim'
 processed_data_dir = project_dir / 'data' / 'processed'
 
 # LINK TO RAW DATA SOURCE
